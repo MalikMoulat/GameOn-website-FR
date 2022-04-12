254cfb9658ac68fca11ecb1b66af0e8963e382bf
MalikMoulat <malik.moulat@hotmail.fr>
2022-04-12 16:35:33 +0200

    Close modal ok

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,12 +12,19 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+const closeBtn = document.querySelectorAll(".close"); // Variable pour le bouton clode du modal 
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+function closeModal(){
+  modalbg.style.display = "none";
 }
 
 
