f3546a2346cdf48b398c4c746e4736f92cf8f1d3
MalikMoulat <malik.moulat@hotmail.fr>
2022-04-16 01:30:33 +0200

    Implémentation des entrée du formulaire + messages d'erreur

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -29,3 +29,115 @@ function closeModal(){
 }
 
 
+
+// Vérification des données du formulaire 
+document.getElementById("inscription").addEventListener("submit", function(e) {
+
+  // Variable erreur
+  let error;
+
+  // DOM Elements form subscribe
+  let firstName = document.getElementById("first");
+  let lastName = document.getElementById("last");
+  let email = document.getElementById("email");
+  let birthDate = document.getElementById("birthdate");
+  let numberOfTournaments = document.getElementById("quantity");
+  let tournamentTown = document.querySelector('input[name="location"]:checked');
+
+  
+  
+
+  // Vérification firstName 
+  if (!firstName.value){  // Si la valeur de la variable est vide
+    error = 'Veuillez renseigner un prénom.'
+    document.getElementById("errorFirst").innerHTML = error;
+    first.style.border = "2px solid red";
+  }else if (firstName.value.length == 1 || firstName.value.length < 2){ // Si value.length == 1 ou < 2
+    error = 'Veuillez entrer 2 caractères ou plus pour le champ du prénom.'
+    document.getElementById("errorFirst").innerHTML = error;
+    first.style.border = "2px solid red";
+  }else{  // reset le style border et le message si il y a eu une première entrée avec une erreur
+    first.style.border = '';
+    document.getElementById("errorFirst").innerHTML = '';
+  }
+
+  
+
+  // Vérification valeur de lastName existe
+  if (!lastName.value){ // Si la valeur de la variable est vide
+    error = 'Veuillez renseigner un nom.'
+    document.getElementById("errorLast").innerHTML = error;
+    last.style.border = "2px solid red";
+  }else if (lastName.value.length == 1 || lastName.value.length < 2){ // Si value.length == 1 ou < 2
+    error = 'Veuillez entrer 2 caractères ou plus pour le champ du nom.'
+    document.getElementById("errorLast").innerHTML = error;
+    last.style.border = "2px solid red";
+  }else{  // reset le style border et le message si il y a eu une première entrée avec une erreur
+    last.style.border = '';
+    document.getElementById("errorLast").innerHTML = '';
+  }
+
+  // Vérification valeur de email existe
+  if (!email.value){
+    error = 'Veuillez renseigner une adresse mail.'
+    document.getElementById("errorMail").innerHTML = error;
+    email.style.border = "2px solid red";
+  }else{  // reset le style border et le message si il y a eu une première entrée avec une erreur
+    email.style.border = '';
+    document.getElementById("errorMail").innerHTML = '';
+  }
+
+// Vérification valeur de birthDate existe
+  if (!birthDate.value){
+    error = 'Vous devez entrer votre date de naissance.'
+    document.getElementById("errorBirthDate").innerHTML = error;
+    birthdate.style.border = "2px solid red";
+  }else{  // reset le style border et le message si il y a eu une première entrée avec une erreur
+    birthdate.style.border = '';
+    document.getElementById("errorBirthDate").innerHTML = '';
+  }
+
+  // Vérification valeur de numberOfTournaments existe
+  if (!numberOfTournaments.value){
+    error = 'Veuillez renseigner un nombre entre 0 et 99.'
+    document.getElementById("errorQuantity").innerHTML = error;
+    quantity.style.border = "2px solid red"; 
+  }else{  // reset le style border et le message si il y a eu une première entrée avec une erreur
+    quantity.style.border = '';
+    document.getElementById("errorQuantity").innerHTML = '';
+  }
+
+  // Vérification valeur de tournamentTown existe
+  if (!tournamentTown){
+    error = 'Vous devez choisir une option.';
+    document.getElementById("errorCheckLocation").innerHTML = error;
+    
+  }else{  // reset le message si il y a eu une première entrée avec une erreur
+    document.getElementById("errorCheckLocation").innerHTML = '';
+  }
+
+  // Vérification id = checkbox1 DOM coché
+  if (document.getElementById("checkbox1").checked !== true)
+	{
+    error = "Vous devez vérifier que vous acceptez les termes et conditions."
+    document.getElementById("errorCheckbox1").innerHTML = error;
+	}else{  // reset le message si il y a eu une première entrée avec une erreur
+    document.getElementById("errorCheckbox1").innerHTML = '';
+  }
+
+ 
+
+
+  // Si la variable error est appelée le formulaire ne s'envoie pas sinon OK
+  if (error){
+    e.preventDefault();
+    return false;
+  }else{
+    alert('formulaire envoyé !');
+  }
+
+  
+
+});
+
+
