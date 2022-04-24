fbb6fe61c63284ddbbfb6d70ca4d8c63d4e65a2d
MalikMoulat <malik.moulat@hotmail.fr>
2022-04-24 23:34:06 +0200

    fix regex email

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -56,7 +56,7 @@ const numberOfTournamentsRed = 'quantity'; // ID de l'input
 
 
 //Regex
-const regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 const regexName = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/;
 
 
